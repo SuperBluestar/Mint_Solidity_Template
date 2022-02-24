35b13b5e338a7211991af39d403baee6254fa1a3
SuperBluestar <alexander116gm@gmail.com>
2022-02-24 09:52:00 +0900

    update port

--- mint-backend/src/main.ts
+++ mint-backend/src/main.ts
@@ -2,11 +2,13 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+require("dotenv").config();
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
-  await app.listen(5000);
+  await app.listen(process.env.PORT || 80);
 }
 bootstrap();
