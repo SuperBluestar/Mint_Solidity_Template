2968ebbf6d5bd708fe18d0898298a4d72f475d38
SuperBluestar <alexander116gm@gmail.com>
2022-02-24 22:05:06 +0900

    Update app-cluster.service.ts

--- mint-backend/src/app-cluster.service.ts
+++ mint-backend/src/app-cluster.service.ts
@@ -1,5 +1,5 @@
-import cluster from 'cluster';
-import os from 'os';
+import * as cluster from 'cluster';
+import * as os from 'os';
 import { Injectable } from '@nestjs/common';
 
 const numCPUs = os.cpus().length;
@@ -7,14 +7,14 @@ const numCPUs = os.cpus().length;
 @Injectable()
 export class AppClusterService {
     static clusterize(callback: Function): void {
-        if(cluster.isMaster){
+        if(cluster.default.isMaster){
             console.log(`Master server started on ${process.pid}`);
             for (let i = 0; i < numCPUs; i++) {
-                cluster.fork();
+                cluster.default.fork();
             }
-            cluster.on('exit', (worker, code, signal) => {
+            cluster.default.on('exit', (worker, code, signal) => {
                 console.log(`Worker ${worker.process.pid} died. Restarting`);
-                cluster.fork();
+                cluster.default.fork();
             })
         } else {
             console.log(`Cluster server started on ${process.pid}`)
