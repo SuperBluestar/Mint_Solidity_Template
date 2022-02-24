9fce025b655ecf5e6a29ea27d7c5f4e9d8816282
SuperBluestar <alexander116gm@gmail.com>
2022-02-24 22:51:41 +0900

    add helmet and title

--- mint-frontend/src/AppRoutes.tsx
+++ mint-frontend/src/AppRoutes.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Routes, Route, useLocation, Navigate } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 import Footer from "layouts/Footer";
 
@@ -14,6 +15,35 @@ const AppRoutes = () => {
     const location = useLocation();
     return (
         <div className="w-full flex flex-col">
+            <Helmet>
+                <meta charSet="utf-8" />
+                <meta name="title" content="Ukrainecharity"/>
+                <meta name="description" content="Pray for Ukraine - Your help will be seen!"/>
+                {/* <meta name="author" content="The Mafioso"/> */}
+                <meta name="image" content="IMAGE SENT BELOW"/>
+                <link rel="apple-touch-icon" href="/logo192.png"/>
+                <link rel="manifest" href="/manifest.json"/>
+                {/* <link rel="stylesheet" href="/font/index.css"/> */}
+                <title>Ukrainecharity</title>
+                <meta property="og:title" content="Ukrainecharity"/>
+                <meta property="og:site_name" content="Ukrainecharity"/>
+                <meta property="og:url" content="https://ukrainecharity.gives/"/>
+                {/* <meta property="article:author" content="The Mafioso™"/>
+                <meta property="og:description" content="The #AAA hyper-real mafia #metaverse on #fantom Find fame, glory and real power in the battle for the ultimate title: The Mafioso"/>
+                <meta property="og:type" content="article"/>
+                <meta property="article:tag" content="The Mafioso™, nft, art, crypto, blockchain, bnb, eth, minted, bsc, opensea, artist, nft creator, nft marketplace opensea, marketplace"/>
+                <meta property="og:image" content="https://themafioso.com/title_slide.png" />
+                <meta property="og:type" content="website"/>
+                <meta property="og:url" content="https://themafioso.com/"/> */}
+                <meta property="og:title" content="Ukrainecharity"/>
+                {/* <meta property="og:description" content="The #AAA hyper-real mafia #metaverse on #fantom Find fame, glory and real power in the battle for the ultimate title: The Mafioso"/>
+                <meta property="og:image" content="https://themafioso.com/title_slide.png" />
+                <meta property="twitter:card" content="summary_large_image"/>
+                <meta property="twitter:url" content="https://www.thejiggys.com/"/> */}
+                <meta property="twitter:title" content="Ukrainecharity"/>
+                {/* <meta property="twitter:description" content="The #AAA hyper-real mafia #metaverse on #fantom Find fame, glory and real power in the battle for the ultimate title: The MafiosoL"/>
+                <meta property="twitter:image" content="https://themafioso.com/title_slide.png" /> */}
+            </Helmet>
             <Routes location={location}>
                 <Route path="" element={<Home />} />
                 <Route path="admin" element={<Admin />}>
